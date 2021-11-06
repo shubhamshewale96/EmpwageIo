89537c05e6490d146c1a6e8d13cb6e95565cd96e
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-11-06 07:17:06 +0530

    welcome to Emplyewage payroll problem IO operation

---
+++ src/main/java/EmployeWageIo/Main.java
@@ -0,0 +1,9 @@
+package EmployeWageIo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employewage Payroll problem io opretion");
+	}
+}
